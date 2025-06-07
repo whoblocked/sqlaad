8e1ba6b9bc5af2df23e4ba69d2268e493c085f8d
whoblocked <morph8y@gmail.com>
2025-06-07 17:50:02 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+  darkMode: 'class', // <-- очень важно
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
